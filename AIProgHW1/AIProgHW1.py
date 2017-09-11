5da0c1d81cc1a96a532168214c640d61f1382fd4
Morgan Knoch <morgankn11@gmail.com>
2017-09-11 16:34:53 -0500

    first commit

---
+++ AIProgHW1/AIProgHW1.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+a = np.array([[1,2,3],[4,5,6],[7,8, '.']])
+
+
+class GameBoard(object):
+    pass
+
+
+def IDS():
+    pass
+
+def DFGS():
+    pass
+
+def Astar():
+    pass
+
+def legalMoves():
+    pass
